c659dbf9804c38975e7a189952df04a63cbd1ec2
Javier Cruz <javi3rcruz1999@gmail.com>
2025-02-26 00:29:00 -0800

    fixed the unordered map for Tokens and chaged the lexer code

--- project1.cpp
+++ project1.cpp
@@ -10,27 +10,46 @@
 using namespace std;
 
 // Define token types
-unordered_map<string, string> TOKENS = {
-    {"leftParen", "("}, {"rightParen", ")"}, {"leftBracket", "["}, {"rightBracket", "]"},
-    {"leftBrace", "{"}, {"rightBrace", "}"}, {"dot", "."}, {"plus", "+"}, {"minus", "-"},
-    {"multiply", "*"}, {"divide", "/"}, {"modulus", "%"}, {"lessThan", "<"},
-    {"greaterThan", ">"}, {"assignment", "="}, {"semicolon", ";"}, {"comma", ","},
-    {"increment", "++"}, {"decrement", "--"}, {"lessThanEq", "<="}, {"greaterThanEq", ">="},
-    {"logicEqual", "=="}, {"logicAnd", "&&"}, {"logicOr", "||"}, {"logicNot", "!"},
-    {"bitAnd", "&"}, {"bitOr", "|"}
+unordered_map<string, string> TOKENS = {  // I put them in wrong order pair 
+    {"(", "leftParen"}, {")", "rightParen"}, {"[", "leftBracket"}, {"]", "rightBracket"},
+    {"{", "leftBrace"}, {"}", "rightBrace"}, {".", "dot"}, {"+", "plus"}, {"-", "minus"},
+    {"*", "multiply"}, {"/", "divide"}, {"%", "modulus"}, {"<", "lessThan"},
+    {">", "greaterThan"}, {"=", "assignment"}, {";", "semicolon"}, {",", "comma"},
+    {"++", "increment"}, {"--", "decrement"}, {"<=", "lessThanEq"}, {">=", "greaterThanEq"},
+    {"==", "logicEqual"}, {"&&", "logicAnd"}, {"||", "logicOr"}, {"!", "logicNot"},
+    {"&", "bitAnd"}, {"|", "bitOr"}
 };
 
 // Keywords
 unordered_set<string> KEYWORDS = {"int", "return", "if", "switch", "float", "while", "else", "case",
                                   "char", "for", "goto", "unsigned", "main", "break", "continue", "void"};
 
-void lexicalAnalyzer(const std::string filename, const std::unordered_map<std::string, std::string>& Tokens, 
-    unordered_set<string> KEYWORDSconst)
+std::vector<std::pair<std::string, std::string>> lexicalAnalyzer(const std::string fileName)
 {
-    ifstream file(filename);
-    if(!file)
+    ifstream file(fileName);  // Extracts info from file
+    if(!file)  // Error test case 
     {
         std::cerr << "Error opening file!";
+        return;
+    }
+
+    std::string currentLine; // String for containing the current line from the file
+    while(getline(file, currentLine)) // Looping through each line
+    {
+        std::istringstream lineStream(currentLine); // Line is taken from file and contained in string currentLine
+        std::string token; // String to hold current token
+        while(lineStream >> token) // Need to loop through the string to get each token and check type
+        {
+            // check if number
+
+            // check if token 
+
+            // check if keyword
+
+            //check if operator 
+
+            // look for errors?
+        }
     }
 
 }
